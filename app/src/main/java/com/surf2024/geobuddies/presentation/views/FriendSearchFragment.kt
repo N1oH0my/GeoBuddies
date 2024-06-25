dfbabee2bfda4acbcaf3b56e8cb47c42841dd500
N1oH0my <h0myn1o@mail.ru>
2024-06-25 18:19:02 +0300

    optimization RV with ListAdapter and DiffUtil.ItemCallback

--- app/src/main/java/com/surf2024/geobuddies/presentation/views/FriendSearchFragment.kt
+++ app/src/main/java/com/surf2024/geobuddies/presentation/views/FriendSearchFragment.kt
@@ -84,6 +84,9 @@ class FriendSearchFragment : Fragment(), IOnFriendItemClickListener {
     private fun initRecyclerView(){
         recyclerView = binding.foudFriendRecyclerview
         recyclerView.layoutManager = LinearLayoutManager(context)
+
+        adapter = FriendSearchRVAdapter(requireContext(), this)
+        recyclerView.adapter = adapter
     }
     private fun initListenerSearchFriendField() {
         binding.editTextSearch.editText?.setOnEditorActionListener { v, actionId, event ->
@@ -101,9 +104,7 @@ class FriendSearchFragment : Fragment(), IOnFriendItemClickListener {
 
     private fun initObserversFriendSearchViewModel() {
         friendSearchViewModel.foundFriendList.observe(viewLifecycleOwner){  foundFriendList ->
-            adapter = FriendSearchRVAdapter(requireContext(), foundFriendList, this)
-            recyclerView.adapter = adapter
-
+            adapter.reload(foundFriendList)
             dataList = foundFriendList
             if (dataList.isEmpty()){
                 showToast("no users found or check auth")
